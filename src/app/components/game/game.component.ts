76b6928d37e32b9fb41cc27005266aa6579b8cd5
nexus122 <juan-pablo122@hotmail.es>
2020-03-08 12:35:57 +0100

    Evento click en los resultados para cargar nuevos

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -8,27 +8,39 @@ import { HuntingService } from "../../services/hunting.service";
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  name:string;
-  links:any[] = [];
-  nombres:any[] = [];
-  index:number = 0;
-  constructor( private route:ActivatedRoute,
-               private huntingService:HuntingService
-             ) { 
+  name: string;
+  title: string;
+  links: any[] = [];
+  nombres: any[] = [];
+  index: number = 0;
+  constructor(private route: ActivatedRoute,
+    private huntingService: HuntingService
+  ) {
 
-    this.route.params.subscribe(params=>{
-      this.name = params.name;      
+    this.route.params.subscribe(params => {
+      this.name = params.name;
     })
 
-    this.huntingService.getRandom( this.name ).subscribe(data =>{
-      console.log(data);   
+    /* Al cargar la pagina se busca una pagina aleatoria */
+    this.huntingService.getRandom(this.name).subscribe(data => {
+      console.log(data);
       this.nombres = data[0];
-      this.links = data[1];  
-  })
+      this.links = data[1];
+      this.title = data[2];
+    })
 
-   }
+  }
 
   ngOnInit() {
   }
 
+  getPage(link: string, name:string) {
+    /* Buscamos una pagina concreta */
+    this.huntingService.getPage(link, name).subscribe(data => {
+      console.log(data);
+      this.nombres = data[0];
+      this.links = data[1];
+      this.title = data[2];
+    })
+  }
 }
--- src/app/services/hunting.service.ts
+++ src/app/services/hunting.service.ts
@@ -7,16 +7,30 @@ import { HttpClient } from '@angular/common/http';
 
 export class HuntingService {
   name:string;
+
   constructor( private http: HttpClient ) { 
     console.log(" Servidor de scraping listo ! ");
-
   }
 
   getRandom( name:string ){            
 
     console.log("Funcion que busca una pagina aleatoria de wikipedia");
 
-    var data = "Especial:Aleatoria";
+    var data = "/wiki/Especial:Aleatoria";
+    var leng = "es";
+    var winer = name;
+
+    const url = `https://hitler-s-hunt.herokuapp.com/peticion/?data=${data}&&leng=${leng}&&winer=${winer}`;          
+
+    return this.http.get(url); 
+  }
+
+  getPage( link, name ){            
+
+    console.log("Funcion que busca una pagina concreta de wikipedia");
+    console.log(link);
+
+    var data = link;
     var leng = "es";
     var winer = name;
 
